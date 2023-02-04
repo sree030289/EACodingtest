64c639fce81ff0d3422a5fc0381c60e8036ef7e3
sree030289 <sree030289@gmail.com>
2023-02-05 08:03:06 +1100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
     presets: [
         [ '@babel/preset-env', {
             targets: {
+                //node 14 or more
                 node: 14,
             },
         } ],
